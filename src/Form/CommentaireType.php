a791e0490f1cf82e4d84959d3a012512cc8e377c
Strentz <ps@thinkfab.fr>
2022-06-09 22:15:55 +0200

    feature: comments style updated miss logic implementation

--- src/Form/CommentaireType.php
+++ src/Form/CommentaireType.php
@@ -3,8 +3,9 @@
 namespace App\Form;
 
 use App\Dto\CommentaireDto;
-use EasyCorp\Bundle\EasyAdminBundle\Form\Type\TextEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,8 +14,24 @@ class CommentaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('username', TextType::class)
-            ->add('content', TextEditorType::class);
+        $builder->add('username', TextType::class, array(
+            'attr' => array(
+                'class' => 'field--text'
+            ),
+            'label' => 'global.username'
+        ))
+            ->add('content', TextareaType::class, array(
+                'attr' => array(
+                    'class' => 'field--text-area'
+                ),
+                'label' => 'global.your_message'
+            ))
+            ->add('submit', SubmitType::class, array(
+                'attr' => array(
+                    'class' => 'btn field--submit'
+                ),
+                'label' => 'global.submit'
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
