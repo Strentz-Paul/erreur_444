fedb253d54873ef1b10394c5caa3d57445a40464
Strentz <ps@thinkfab.fr>
2022-06-19 15:48:53 +0200

    feature: fix route to features

--- src/Controller/FeaturesController.php
+++ src/Controller/FeaturesController.php
@@ -8,14 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FeaturesController extends AbstractController
 {
-    #[Route('/features/index', name: 'features--live_component')]
+    #[Route('/features', name: 'features_index')]
     public function featuresIndexAction(): Response
     {
         // TODO : ici il faudra creer un enum de php 8.1 pour pouvoir afficher les features
         return $this->render('features/index.html.twig');
     }
 
-    #[Route('/features/live-component/', name: 'features--live_component')]
+    #[Route('/features/live-component/', name: 'features_live_component')]
     public function featureLiveComponentAction(): Response
     {
         return $this->render('features/live-components.html.twig');
