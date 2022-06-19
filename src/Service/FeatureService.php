a3f8b2b219f055b5b95b26f9b963f22221bd9809
Strentz <ps@thinkfab.fr>
2022-06-19 21:50:32 +0200

    feature: update design and link features in menu

--- src/Service/FeatureService.php
+++ src/Service/FeatureService.php
@@ -2,13 +2,35 @@
 
 namespace App\Service;
 
+use App\Enum\FeaturesEnum;
+use App\ViewModel\FeaturesVm;
 use App\Contracts\Service\FeaturesServiceInterface;
-use App\Contracts\Service\FeaturesVM;
+use App\ViewModel\FeatureVM;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class FeatureService implements FeaturesServiceInterface
 {
+    public function __construct(
+        private TranslatorInterface $translator
+    ){}
+
+    /**
+     * @return FeaturesVm
+     */
     public function getFeatures(): FeaturesVM
     {
-        // TODO: Implement getFeatures() method.
+        $features = new FeaturesVm();
+        $enums = FeaturesEnum::values();
+        foreach ($enums as $e) {
+            match ($e) {
+                FeaturesEnum::LIVE_COMPONENTS->value => $feature = new FeatureVM(
+                    $this->translator->trans('feature.live_component.title'),
+                    $this->translator->trans('feature.live_component.description'),
+                    'features_live_component'
+                )
+            };
+            $features->addFeature($feature);
+        }
+        return $features;
     }
 }
