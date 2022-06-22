a6ca314682db8361f6dab7153058ceb7cd0cbb97
Strentz <ps@thinkfab.fr>
2022-06-22 13:43:04 +0200

    feature: add markdown editor to live component need to improve

---
+++ src/Components/LiveTextEditorComponent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Components;
+
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\ValidatableComponentTrait;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+#[AsLiveComponent('live_text_editor')]
+class LiveTextEditorComponent
+{
+    use DefaultActionTrait;
+    use ValidatableComponentTrait;
+
+    #[LiveProp]
+    public string $name;
+
+    #[LiveProp]
+    public string $label;
+
+    #[LiveProp(writable: true)]
+    #[Assert\NotBlank]
+    public string $value = '';
+
+    public function mount(string $name): void
+    {
+        $this->name = $name;
+        $this->label = ucfirst($name);
+    }
+
+//    public function getRows(): int
+//    {
+//        return max(3, floor(strlen($this->value)));
+//    }
+}
