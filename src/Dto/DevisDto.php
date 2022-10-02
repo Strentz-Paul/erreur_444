fe54efc798a21a33f54343ee682b24d785110011
Paul Strentz <ps@thinkfab.fr>
2022-10-02 12:44:19 +0200

    feature:devis in progress

---
+++ src/Dto/DevisDto.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Dto;
+
+use App\Entity\Entreprise;
+use App\Enum\DevisStatutEnum;
+
+class DevisDto
+{
+    private ?int $nbJours;
+    private ?float $tjm;
+    private DevisStatutEnum $statut;
+    private bool $completed = false;
+    private Entreprise $entreprise;
+
+    /**
+     * @return DevisStatutEnum
+     */
+    public function getStatut(): DevisStatutEnum
+    {
+        return $this->statut;
+    }
+
+    /**
+     * @param string $statut
+     * @return DevisDto
+     */
+    public function setStatut(string $statut): DevisDto
+    {
+        $this->statut = DevisStatutEnum::from($statut);
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getNbJours(): ?int
+    {
+        return $this->nbJours;
+    }
+
+    /**
+     * @param int|null $nbJours
+     * @return DevisDto
+     */
+    public function setNbJours(?int $nbJours): DevisDto
+    {
+        $this->nbJours = $nbJours;
+        return $this;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getTjm(): ?float
+    {
+        return $this->tjm;
+    }
+
+    /**
+     * @param float|null $tjm
+     * @return DevisDto
+     */
+    public function setTjm(?float $tjm): DevisDto
+    {
+        $this->tjm = $tjm;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCompleted(): bool
+    {
+        return $this->completed;
+    }
+
+    /**
+     * @param bool $completed
+     * @return DevisDto
+     */
+    public function setCompleted(bool $completed): DevisDto
+    {
+        $this->completed = $completed;
+        return $this;
+    }
+
+    /**
+     * @return Entreprise
+     */
+    public function getEntreprise(): Entreprise
+    {
+        return $this->entreprise;
+    }
+
+    /**
+     * @param Entreprise $entreprise
+     * @return DevisDto
+     */
+    public function setEntreprise(Entreprise $entreprise): DevisDto
+    {
+        $this->entreprise = $entreprise;
+        return $this;
+    }
+}
